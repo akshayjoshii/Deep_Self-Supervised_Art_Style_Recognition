610ebff1de1b58a43efef1f30b02c765e5c7a773
Akshay Joshi <akshayjoshi56@gmail.com>
2020-06-30 07:23:57 +0200

    first

--- Create_Dataset.py
+++ Create_Dataset.py
@@ -6,10 +6,10 @@ from tqdm import tqdm
 import pickle
 import random
 
-#Comment
+#Comment one 2 3
 Datapath = "/src/wikiart"
 Categories = ["Abstract_Expressionism", "Action_painting", "Analytical_Cubism", "Art_Nouveau_Modern", 
-                "Baroque", "Color_Field_Painting", "Contemporary_Realism", "Cubism", "Early_Renaissance", "Expressionism",
+                "Baroque", "Color_Field_Paigit nting", "Contemporary_Realism", "Cubism", "Early_Renaissance", "Expressionism",
                 "Fauvism", "High_Renaissance", "Impressionism", "Mannerism_Late_Renaissance", "Minimalism", "Naive_Art_Primitivism", 
                 "New_Realism", "Northern_Renaissance", "Pointillism", "Pop_Art", "Post_Impressionism", "Realism", "Rococo", "Romanticism",
                 "Symbolism", "Synthetic_Cubism", "Ukiyo_e"]
